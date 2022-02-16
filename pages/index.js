274c2c386f54ac25a45112bbede1eac882cc782a
Quentin Lintz <quentinlintz@outlook.com>
2022-02-15 22:30:23 -0500

    Add text and begin to position it

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[#161413] min-h-screen'>
       <Image
         src='https://i.postimg.cc/Y0mdjrJq/IMG-2728.jpg'
         alt='Quentin'
@@ -10,6 +10,14 @@ export default function Home() {
         width='2048'
         layout='responsive'
       />
+      <div className='absolute top-0 left-0 px-40 py-20'>
+        <h1 className='mb-3 text-6xl font-semibold tracking-tight text-white'>
+          Hello, I&apos;m Quentin
+        </h1>
+        <h3 className='text-2xl leading-normal text-gray-100'>
+          welcome to my homepage.
+        </h3>
+      </div>
     </div>
   );
 }
