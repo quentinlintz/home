98dcb0441f940f7665127aef2a5cb891013dd22b
Quentin Lintz <quentinlintz@outlook.com>
2022-02-17 22:08:44 -0500

    Componentize HeaderIcon and add framer-motion animation

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,6 @@
-import Link from 'next/link';
 import React from 'react';
-import { Icon, Flex, Spacer } from '@chakra-ui/react';
+import HeaderIcon from '../HeaderIcon/HeaderIcon';
+import { Flex, Spacer } from '@chakra-ui/react';
 import {
   AiFillGithub,
   AiFillTwitterCircle,
@@ -10,29 +10,17 @@ import {
 const Header = () => (
   <Flex p={['4', '8']} w='100%' bgGradient='linear(to-t, #171313, black)'>
     <Spacer />
-    <Link href='https://github.com/quentinlintz' passHref>
-      <Icon
-        as={AiFillGithub}
-        boxSize={{ base: '24px', md: '40px', lg: '56px' }}
-        color='white'
-      />
-    </Link>
+    <HeaderIcon link='https://github.com/quentinlintz' icon={AiFillGithub} />
     <Spacer />
-    <Link href='https://twitter.com/quentinlintz' passHref>
-      <Icon
-        as={AiFillTwitterCircle}
-        boxSize={{ base: '24px', md: '40px', lg: '56px' }}
-        color='white'
-      />
-    </Link>
+    <HeaderIcon
+      link='https://twitter.com/quentinlintz'
+      icon={AiFillTwitterCircle}
+    />
     <Spacer />
-    <Link href='https://linkedin.com/in/quentinlintz/' passHref>
-      <Icon
-        as={AiFillLinkedin}
-        boxSize={{ base: '24px', md: '40px', lg: '56px' }}
-        color='white'
-      />
-    </Link>
+    <HeaderIcon
+      link='https://linkedin.com/in/quentinlintz'
+      icon={AiFillLinkedin}
+    />
     <Spacer />
   </Flex>
 );
---
+++ components/HeaderIcon/HeaderIcon.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import { Icon } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+const HeaderIcon = ({ link, icon }) => (
+  <motion.div
+    initial='hidden'
+    animate='visible'
+    variants={item}
+    transition={{ duration: 2 }}
+    style={{ cursor: 'pointer' }}
+  >
+    <Link href={link} passHref>
+      <Icon
+        as={icon}
+        boxSize={{ base: '24px', md: '40px', lg: '56px' }}
+        color='white'
+      />
+    </Link>
+  </motion.div>
+);
+
+export default HeaderIcon;
