b1616339a4a1670aee723ab0827fb18df67b3ac3
Quentin Lintz <quentinlintz@outlook.com>
2022-02-21 13:03:33 -0500

    Remove theme.js and declare theme in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from './theme';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: '#171313',
+      },
+    }),
+  },
+  fonts: {
+    heading: 'Josefin Sans, sans-serif',
+    body: 'Josefin Sans, sans-serif',
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
