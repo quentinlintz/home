28211fc18f177bcf49d3f63ed347890024b69d24
Quentin Lintz <quentinlintz@outlook.com>
2022-02-21 13:08:34 -0500

    Add title to Head component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
+import Head from 'next/head';
 import Header from '../components/Header/Header';
 import Welcome from '../components/Welcome/Welcome';
 import { Box } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <Box>
-      <Header />
-      <Welcome />
-    </Box>
+    <div>
+      <Head>
+        <title>Home • Quentin Lintz</title>
+      </Head>
+      <Box>
+        <Header />
+        <Welcome />
+      </Box>
+    </div>
   );
 }
