683290d493247bf4d7530a4430af9e5af9a8443c
Quentin Lintz <quentinlintz@outlook.com>
2022-03-17 18:54:25 -0400

    Remove next-share and add navigator.share

--- components/wami/EndModal.js
+++ components/wami/EndModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {
   Box,
+  Button,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -11,29 +12,27 @@ import {
   ModalCloseButton,
   HStack,
 } from '@chakra-ui/react';
-import moment from 'moment';
-import {
-  FacebookShareButton,
-  FacebookIcon,
-  TwitterShareButton,
-  TwitterIcon,
-  LinkedinShareButton,
-  LinkedinIcon,
-} from 'next-share';
 import Countdown from 'react-countdown';
+import { BsFillShareFill } from 'react-icons/bs';
 
 import { scoreToEmoji } from '../common';
 
 const EndModal = ({ isOpen, onClose, victory, challengeData, numGuess }) => {
   const { answer, date, hint } = challengeData || '';
   const score = victory ? numGuess + 1 : 9;
-  const url = 'https://quentinlintz.com/wami';
-  const quote = `WAMI ${date} • Guess the Word • My score: ${score}/8`;
-  const title = `WAMI ${date} • Guess the Word
+  const title = `WAMI ${date} • Guess the Word • My score: ${score}/8`;
+  const text = `WAMI ${date} • Guess the Word
 My score: ${scoreToEmoji(score)}/8️⃣
 
 #WAMI`;
+  const url = 'https://quentinlintz.com/wami';
   const hashtag = '#WAMI';
+
+  const shareData = {
+    title,
+    text,
+    url,
+  };
 
   let today = new Date();
   const minuteOffset = today.getTimezoneOffset();
@@ -41,6 +40,12 @@ My score: ${scoreToEmoji(score)}/8️⃣
   today.setHours(0);
   today.setMinutes(minuteOffset * -1);
   today.setSeconds(0);
+
+  const handleClick = async () => {
+    if (navigator.share) {
+      await navigator.share(shareData);
+    }
+  };
 
   return (
     <Modal
@@ -72,22 +77,17 @@ My score: ${scoreToEmoji(score)}/8️⃣
           </>
         )}
         <ModalFooter color='white'>
-          <HStack w='100%'>
-            <Box fontSize='2xl' w='50%'>
-              <Countdown date={today} daysInHours />
-            </Box>
-            <HStack w='50%' spacing={['4', '6']}>
-              <FacebookShareButton url={url} quote={quote} hashtag={hashtag}>
-                <FacebookIcon size={[32, 48]} round />
-              </FacebookShareButton>
-              <TwitterShareButton url={url} title={title}>
-                <TwitterIcon size={[32, 48]} round />
-              </TwitterShareButton>
-              <LinkedinShareButton url={url} summary={title} source={url}>
-                <LinkedinIcon size={[32, 48]} round />
-              </LinkedinShareButton>
-            </HStack>
-          </HStack>
+          <Box fontSize='2xl' w='50%'>
+            <Countdown date={today} daysInHours />
+          </Box>
+          <Button
+            rightIcon={<BsFillShareFill />}
+            size='lg'
+            colorScheme='whiteAlpha'
+            onClick={handleClick}
+          >
+            SHARE
+          </Button>
         </ModalFooter>
         <ModalCloseButton color='white' />
       </ModalContent>
