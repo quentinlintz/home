c6157663164330e6f95baf432585cbc34101eda7
Quentin Lintz <quentinlintz@outlook.com>
2022-03-20 15:07:32 -0400

    Add acknowledgement statement for anonymized analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'focus-visible/dist/focus-visible';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import CookieConsent from 'react-cookie-consent';
 
 const theme = extendTheme({
   styles: {
@@ -19,6 +20,22 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <CookieConsent
+        location='bottom'
+        buttonText='I understand'
+        cookieName='gaConsent'
+        style={{ background: '#424242' }}
+        buttonStyle={{
+          background: '#212121',
+          color: 'white',
+          fontSize: '1em',
+          borderRadius: '0.5em',
+        }}
+        expires={150}
+      >
+        This website collects anonymous page view data and WAMI scores only for
+        statistical purposes.
+      </CookieConsent>
     </ChakraProvider>
   );
 }
